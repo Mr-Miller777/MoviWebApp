c2a9e805064e8d804fb128115d8ede8b41762297
Viacheslav Miller <vaceslav_miller@mail.ru>
2026-08-19 11:22:38 +0200

    User and movie models defined.

---
+++ models.py
@@ -0,0 +1,29 @@
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import backref
+
+db = SQLAlchemy
+
+
+class User(db.Model):
+    __tablename__ = 'user'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False, unique=True)
+    movies = db.relationship('Movie', backref='user', lazy=True)
+
+    def __repr__(self):
+        return f'<User {self.name}>'
+
+
+class Movie(db.Model):
+    __tabelname__ = 'movie'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(200), nullable=False)
+    director = db.Column(db.String(200))
+    year = db.Column(db.Integer)
+    poster_url = db.Column(db.String(500))
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
+    def __repr__(self):
+        return f'<Movie {self.name} ({self.year})>'
