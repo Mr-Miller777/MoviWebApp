b6c94af38eda548ac580ff80af7eafa13f7da0cc
Viacheslav Miller <vaceslav_miller@mail.ru>
2026-08-20 20:45:16 +0200

    Flask application set up.

--- app.py
+++ app.py
@@ -1,11 +1,28 @@
-from flask import Flask, render_template
+import os
+from flask import Flask
+from models import db, User, Movie
+from data_manager import DataManager
 
 app = Flask(__name__)
 
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+DATA_DIR = os.path.join(BASE_DIR, 'data')
+os.makedirs(DATA_DIR, exist_ok=True)
+
+DB_PATH = os.path.join(DATA_DIR, 'movies.db')
+app.config['SQLALCHEMY_DATABASE_URL'] = 'sqlite:///' + DB_PATH
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db.init_app(app)
+
+data_manager = DataManager()
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    return "Welcome to MoviWeb App!"
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5002)
+    with app.app_context():
+        db.create_all()
+    app.run(debug=True)
+    #app.run(debug=True, host='0.0.0.0', port=5002)
